b3c61bbb2cb5c110e5748381b6939125fe6fbc5c
FangBinwei <fangbinwei@qq.com>
2017-08-22 01:27:57 +0800

    修改manage下的子路由,用router-view的name属性来加载

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,7 @@ import User from '@/view/User'
 import ArticleManage from '@/view/manage/ArticleManage'
 import CategoryManage from '@/view/manage/CategoryManage'
 import CommentManage from '@/view/manage/CommentManage'
-import DragtManage from '@/view/manage/DraftManage'
+import DraftManage from '@/view/manage/DraftManage'
 import BlogManage from '@/view/manage/BlogManage'
 import RecycleManage from '@/view/manage/RecycleManage'
 import Router from 'vue-router'
@@ -24,41 +24,57 @@ export default new Router({
           name: 'manage',
           path: 'manage',
           component: Manage,
-          children: [
-            {
-              name: 'article-manage',
-              path: 'article-manage',
-              component: ArticleManage
-            },
-            {
-              name: 'category-manage',
-              path: 'category-manage',
-              component: CategoryManage
-            },
-            {
-              name: 'comment-manage',
-              path: 'comment-manage',
-              component: CommentManage
-            },
-            {
-              name: 'blog-manage',
-              path: 'blog-manage',
-              component: BlogManage
-            },
-            {
-              name: 'draft-manage',
-              path: 'draft-manage',
-              component: DragtManage
-            },
-            {
-              name: 'recycle-manage',
-              path: 'recycle-manage',
-              component: RecycleManage
+          children: [{
+            name: 'manage-nav',
+            path: 'manage-nav',
+            components: {
+              articleManage: ArticleManage,
+              categoryManage: CategoryManage,
+              commentManage: CommentManage,
+              blogManage: BlogManage,
+              draftManage: DraftManage,
+              recycleManage: RecycleManage
             }
-          ]
+          }]
+          // children: [
+          //   {
+          //     name: 'article-manage',
+          //     path: 'article-manage',
+          //     components: {
+          //       ArticleManage,
+          //       CategoryManage,
+          //       CommentManage
+          //     }
+          //   },
+          //   {
+          //     name: 'category-manage',
+          //     path: 'category-manage',
+          //     component: CategoryManage
+          //   },
+          //   {
+          //     name: 'comment-manage',
+          //     path: 'comment-manage',
+          //     component: CommentManage
+          //   },
+          //   {
+          //     name: 'blog-manage',
+          //     path: 'blog-manage',
+          //     component: BlogManage
+          //   },
+          //   {
+          //     name: 'draft-manage',
+          //     path: 'draft-manage',
+          //     component: DraftManage
+          //   },
+          //   {
+          //     name: 'recycle-manage',
+          //     path: 'recycle-manage',
+          //     component: RecycleManage
+          //   }
+          // ]
         },
         {
-          name: 'editor',
+          name: 'write',
           path: '/write',
           component: Editor
         },
