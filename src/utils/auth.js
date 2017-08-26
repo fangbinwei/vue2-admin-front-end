12d78059e0cf0babb0c17e458717297e3e3fb7b2
FangBinwei <fangbinwei@qq.com>
2017-08-26 22:53:22 +0800

    用来设置cookies的函数,后端暂时不处理前端传送的cookies

---
+++ src/utils/auth.js
@@ -0,0 +1,15 @@
+import Cookies from 'js-cookie'
+
+const tokenKey = 'User-Token'
+export function getToken () {
+  return Cookies.get(tokenKey)
+}
+
+export function setToken (token) {
+  let minutes = new Date(new Date().getTime() + 1000 * 60 * 60 * 24)
+  return Cookies.set(tokenKey, token, {expires: minutes, path: '/'})
+}
+
+export function removeToken () {
+  return Cookies.remove(tokenKey, {path: '/'})
+}
