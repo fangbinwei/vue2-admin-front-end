c646acd24953918b37bad15ffca4048c44114b97
FangBinwei <fangbinwei@qq.com>
2017-09-02 19:53:05 +0800

    markdown test

--- src/filter/index.js
+++ src/filter/index.js
@@ -1,4 +1,6 @@
 import momentJS from 'moment'
+import marked from 'marked'
+import highlight from 'highlight.js'
 
 export function moment (value, formatString) {
   formatString = formatString || 'MM-DD HH:mm:ss'
@@ -7,4 +9,44 @@ export function moment (value, formatString) {
 export function momentWithYear (value, formatString) {
   formatString = formatString || 'YYYY-MM-DD HH:mm:ss'
   return momentJS(value).format(formatString)
+}
+const languages = ['cpp', 'xml', 'bash', 'coffeescript', 'css', 'markdown', 'http', 'java', 'javascript', 'json', 'less', 'makefile', 'nginx', 'php', 'python', 'scss', 'sql', 'stylus']
+highlight.registerLanguage('cpp', require('highlight.js/lib/languages/cpp'))
+highlight.registerLanguage('xml', require('highlight.js/lib/languages/xml'))
+highlight.registerLanguage('bash', require('highlight.js/lib/languages/bash'))
+highlight.registerLanguage('coffeescript', require('highlight.js/lib/languages/coffeescript'))
+highlight.registerLanguage('css', require('highlight.js/lib/languages/css'))
+highlight.registerLanguage('markdown', require('highlight.js/lib/languages/markdown'))
+highlight.registerLanguage('http', require('highlight.js/lib/languages/http'))
+highlight.registerLanguage('java', require('highlight.js/lib/languages/java'))
+highlight.registerLanguage('javascript', require('highlight.js/lib/languages/javascript'))
+highlight.registerLanguage('json', require('highlight.js/lib/languages/json'))
+highlight.registerLanguage('less', require('highlight.js/lib/languages/less'))
+highlight.registerLanguage('makefile', require('highlight.js/lib/languages/makefile'))
+highlight.registerLanguage('nginx', require('highlight.js/lib/languages/nginx'))
+highlight.registerLanguage('php', require('highlight.js/lib/languages/php'))
+highlight.registerLanguage('python', require('highlight.js/lib/languages/python'))
+highlight.registerLanguage('scss', require('highlight.js/lib/languages/scss'))
+highlight.registerLanguage('sql', require('highlight.js/lib/languages/sql'))
+highlight.registerLanguage('stylus', require('highlight.js/lib/languages/stylus'))
+let renderer = new marked.Renderer()
+
+marked.setOptions({
+  renderer: renderer,
+  gfm: true,
+  tables: true,
+  breaks: false,
+  pedantic: false,
+  sanitize: false,
+  smartLists: true,
+  smartypants: false,
+  highlight: function (code, lang) {
+    if (!~languages.indexOf(lang)) {
+      return highlight.highlightAuto(code).value
+    }
+    return highlight.highlight(lang, code).value
+  }
+})
+export function markdown (str) {
+  return marked(str)
 }
